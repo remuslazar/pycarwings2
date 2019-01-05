98b566935f726002ce556580a962fadd0c802e25
Phil Cole <filcole@gmail.com>
2019-01-05 22:36:19 +0000

    Test Python 3.4 support

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
